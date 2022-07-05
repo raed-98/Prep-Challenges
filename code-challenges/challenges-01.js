bc5ee31e8c667a9852edd6d627ad41a52c012458
raed-98 <raedzwate010@gmail.com>
2022-07-06 01:54:01 +0300

    ex01

---
+++ code-challenges/challenges-01.js
@@ -0,0 +1,11 @@
+// input : [12,32,22,45,78,12,50]
+//output : 78
+const findMax = (arr){ 
+    let max;  
+    let  max= arr[0];
+    for(let r=0;r <arr[0];r++)
+    if (arr[r]>=max){
+        max=arr[r];
+    }
+    return max;
+}
